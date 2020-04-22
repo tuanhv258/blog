420aed280d6efe537b7c554d68d1752c4ebe3561
ho.viet.tuan <ho.viet.tuan@sun-asterisk.com>
2020-04-22 21:17:46 +0700

    initial

--- src/config.js
+++ src/config.js
@@ -1,21 +1,21 @@
 export const config = {
   // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
-  githubConvertedToken: "MzM2N2YxZDRjNDk5MmQ4MGVjNzQwYmM5Y2FlZWYyNDE0ZGQ4NWFiYQ==",
+  githubConvertedToken: "YTM5Yjc0NWQ3Y2Y3MmFiZmYzZmQ4YWE4ZWE0ZjlkYjJmZTY4NWU1NA==",
 
   // Your Github UserName
-  githubUserName: "saadpasta",
+  githubUserName: "tuanhv258",
 
   // Your Github Repo Name Where You Have your issues as Blog
-  githubRepo: "react-blog-github",
+  githubRepo: "tuanhv258.github.io",
 
   // Set it to true if you have a Github app to add to this project
   // and fill the client ID & secret
   enableOAuth: true,
-  OAuthClientID: 'Iv1.9f40fd53257d4102',
-  OAuthSecret: 'cc3db76ffe9848c92d83172807eececa79f0eb22',
+  OAuthClientID: 'Iv1.ad856a9b94f0c173',
+  OAuthSecret: '8b801f1b730379c7aa49cd4cf727076689c5809b',
 
   // Your Personal Blog Title
-  title : "Saad Pasta" ,
+  title : "Tuan Ho" ,
 
   // Your Personal Blog Subtitle
   subtitle : "Software Developer",
